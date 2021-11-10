f3f7cfbb9fd0c9eb3a9a312619700320fcd0a2c7
Juan Antonio Silva Flores <39509863+juanantoniosilva90@users.noreply.github.com>
2021-11-10 15:54:14 -0600

    Add files via upload

---
+++ arbol.js
@@ -0,0 +1,21 @@
+
+let hojas="";
+[18,1, 1, 2].forEach(function(a){
+    Array(a).fill("").forEach(function(x, i){
+        hojas+=([...Array(1+i).fill("<span class='colores'>*~*</span>")].join(""))+"<br>"
+    })
+
+}) 
+
+document.querySelector(".arbol").innerHTML=hojas;
+
+//Vamos a ponerle colores
+
+let colores=document.querySelectorAll(".colores");
+function animacion(){
+    for(let i=0; i<colores.length;i++){
+        let tiempo=i/100+1;
+        colores[i].style.animation="color "+ tiempo+"s infinite"
+    }
+}
+window.addEventListener("load", animacion())
